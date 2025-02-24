a906717e0a43101c413566e6aa6bc6d2c89722a4
Leroydeverwoester <144851@student.dcterra.nl>
2025-02-24 09:29:11 +0100

    login functie
    ja

--- app/login.php
+++ app/login.php
@@ -8,6 +8,48 @@ $port = "3306";
 spl_autoload_register(function($class){
     include __DIR__ . "/" . $class . ".php";
 });
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = htmlspecialchars($_POST['username'] ?? '');
+    $password = htmlspecialchars($_POST['password'] ?? '');
+    $errors = [];
+
+    if (empty($errors)) {
+        verify_password();
+    } else {
+        echo '<div style="color: red;">';
+        foreach ($errors as $error) {
+            echo "<p>$error</p>";
+        }
+        echo '</div>';
+    }
+}
+
+function verify_password() {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    // Validate input
+    if (empty($username) || empty($password)) {
+        echo "Gebruikersnaam en wachtwoord zijn verplicht.";
+        exit;
+    }
+    $database = new Database();
+    $conn = $database->getConnection();
+    // Sanitize input
+    $username = htmlspecialchars($username);
+    $password = htmlspecialchars($password);
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username = :username");
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
+    $user = $stmt->fetch();
+    if ($user && password_verify($password, $user['password'])) {
+        echo "U bent ingelogd.";
+    exit;
+    } else {
+    echo "Gebruikersnaam of wachtwoord is onjuist.";
+    }
+}
 ?>
 
 <html>
@@ -22,7 +64,7 @@ spl_autoload_register(function($class){
     </div>
     <div class="container">
         <p>Please fill in this form to login to your account.</p> <br>
-        <form action="index.php" method="post">
+        <form action='' method="post">
         Username: <br>
         <input type="text" name="username" placeholder="Enter your username" required> <br> <br>
         Password: <br>
